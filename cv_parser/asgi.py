a9eb918d741abc02a2df490cb00901f3cc71bd94
venia-min <57226644+Venia-min@users.noreply.github.com>
2024-11-04 23:39:15 +0100

    "Add app parser"

--- cv_parser/asgi.py
+++ cv_parser/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CV_parser project.
+ASGI config for cv_parser project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CV_parser.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cv_parser.settings')
 
 application = get_asgi_application()
