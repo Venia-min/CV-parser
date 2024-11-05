76d82fd73f852552a6144f42c3c3fca3439661c7
venia-min <57226644+Venia-min@users.noreply.github.com>
2024-11-05 23:58:28 +0100

    "Add validators and tests"

---
+++ parser/tests/test_validators.py
@@ -0,0 +1,39 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.exceptions import ValidationError
+
+import pytest
+
+from parser.validators import validate_file_extension, validate_file_size
+
+
+@pytest.mark.parametrize("extension, is_exception_expected", [
+    ("csv", True),
+    ("mp4", True),
+    ("mp3", True),
+    ("pdf", False),
+    ("doc", False),
+    ("docx", False),
+])
+def test_validate_file_extension(extension, is_exception_expected):
+    file = SimpleUploadedFile(name=f"sample-file.{extension}", content=b"Note")
+    if is_exception_expected:
+        with pytest.raises(ValidationError, match="Only PDF and Docx formats are allowed."):
+            validate_file_extension(file)
+    else:
+        assert not validate_file_extension(file)
+
+
+@pytest.mark.parametrize("file_size, is_exception_expected", [
+    (3_097_152, True),
+    (6_097_152, True),
+    (2_097_152, False),
+    (1_097_152, False),
+])
+def test_validate_file_size(file_size, is_exception_expected):
+    file = SimpleUploadedFile(name="sample-file.pdf", content=b"Note")
+    file.size = file_size
+    if is_exception_expected:
+        with pytest.raises(ValidationError, match="Max file size is 2MB"):
+            validate_file_size(file)
+    else:
+        assert not validate_file_size(file)
--- parser/urls.py
+++ parser/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home_page'),
+    path('upload', views.upload_resume, name='upload_resume'),
 ]
