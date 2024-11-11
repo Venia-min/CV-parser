29fcb020b05142c54a9919175351c34ea74491f5
venia-min <57226644+Venia-min@users.noreply.github.com>
2024-11-11 23:27:20 +0100

    "Add extract_resume_info function with boto3, "

--- parser/utils.py
+++ parser/utils.py
@@ -1,6 +1,10 @@
 from django.core.files.uploadedfile import UploadedFile
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
+import json
 import pymupdf
+import boto3
 
 
 def read_uploaded_file(file_object: UploadedFile, file_extension: str) -> str:
@@ -11,3 +15,54 @@ def read_uploaded_file(file_object: UploadedFile, file_extension: str) -> str:
             text += page.get_text()
 
     return text
+
+
+ai_credentials_options = {
+    "region_name": settings.GEN_AI_REGION,
+    "aws_access_key_id": settings.GEN_AI_ACCESS_KEY,
+    "aws_secret_access_key": settings.GEN_AI_SECRET_KEY
+}
+
+
+prompt_data = """
+You are an AI bot designed to act as a professional for parsing resume. You are given a resume and your job is to
+extract the following information from the resume:
+
+1. applicant_name: ""
+2. highest_level_of_education: ""
+3. area_of_study: ""
+4. institution: ""
+5. introduction: ""
+6. skills: string []
+7. english_proficiency_level: ""
+8. experience: [{"employer_mane":"", role:"", duration:""}]
+
+Give the extracted info in JSON format only.
+Note: if the info is not present, leave the field blank.
+"""
+
+
+def extract_resume_info(text: str):
+    try:
+        bedrock = boto3.client("bedrock-runtime", **ai_credentials_options)
+        payload = {
+            "prompt": "[INST]" + prompt_data + "Resume Content::" + text + "[INST]",
+            "max_gen_len": 1024,
+            "temperature": 0.5,
+            "top_p": 0.9
+        }
+        body = json.dumps(payload)
+        model_id = "meta.llama2-70b-chat-v1"
+
+        response = bedrock.invoke_model(
+            body=body,
+            modelId=model_id,
+            accept="application/json",
+            contentType="application/json",
+        )
+
+        response_body = json.loads(response.get("body").read())
+        response_text: str = response_body["generation"]
+
+    except Exception as err:
+        raise ValidationError(f"Something went wrong: {err}", 500)
--- parser/views.py
+++ parser/views.py
@@ -4,7 +4,7 @@ from django.core.files.uploadedfile import UploadedFile
 from django.core.exceptions import ValidationError
 
 from .validators import validate_file_extension, validate_file_size
-from .utils import read_uploaded_file
+from .utils import read_uploaded_file, extract_resume_info
 
 
 def home(request: HttpRequest):
@@ -18,7 +18,7 @@ def upload_resume(request: HttpRequest):
             ext: str = validate_file_extension(file)
             validate_file_size(file)
             file_content = read_uploaded_file(file, ext)
-            print("file_content>>", file_content)
+            data = extract_resume_info(file_content)
         except ValidationError as err:
-            return JsonResponse({"error": err.message})
+            return JsonResponse({"error": err.message}, status=400)
     return JsonResponse({"error": "Invalid Request"}, status=400)
